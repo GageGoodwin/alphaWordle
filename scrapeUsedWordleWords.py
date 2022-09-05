b60c88a3999ad4bc5a31a35e8a399d0537c84d6c
Gage Goodwin <gage@Gages-MacBook-Air.local>
2022-09-05 14:36:41 -0500

    Wrote the code to scrape all wordle words that have been previous answers

---
+++ scrapeUsedWordleWords.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+r = requests.get('https://www.rockpapershotgun.com/wordle-past-answers')
+soup = BeautifulSoup(r.content, 'html.parser')
+
+prevUsedWords = []
+for ultag in soup.find_all('ul', {'class': 'inline'}):
+    for litag in ultag.find_all('li'):
+        prevUsedWords.append(litag.text)
+
+with open('./{}.txt'.format("words"), mode='wt', encoding='utf-8') as file:
+    for i in prevUsedWords:
+        file.write(i+"\n")
+
